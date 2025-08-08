1e6414e748b4d1d90208fa69b4d46e7490d51495
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-08-08 15:39:04 +0700

    feat : option transaksi langsung

--- app/Http/Controllers/KepalaToko/TransaksiServisLangsungController.php
+++ app/Http/Controllers/KepalaToko/TransaksiServisLangsungController.php
@@ -29,7 +29,7 @@ class TransaksiServisLangsungController extends Controller
     public function store(Request $request)
     {
         // dd($request->all()); 
-        $nomor_servis = '' . mt_rand(date('Ymd00'), date('Ymd99'));
+        $nomor_servis = '' . mt_rand(date('Ymd00'), date('Ymd99')).rand(10,90);
         $nama_pelanggan = Customer::find($request->customers_id);
 
         $nama_tipe = Type::find($request->types_id);
