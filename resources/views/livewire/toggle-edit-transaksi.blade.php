5545dcf753ca37351efa10e9556136702a5d9af8
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-08-10 23:52:10 +0700

    fix : update narasi setting pengaturan toko

--- resources/views/livewire/toggle-edit-transaksi.blade.php
+++ resources/views/livewire/toggle-edit-transaksi.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ is_edit_transaksi: @entangle('is_edit_transaksi') }">
     <h3 class="text-xl leading-snug text-slate-800 font-bold mb-1">Akses Edit Transaksi</h3>
-    <div class="text-sm">Dengan mengaktifkan ini maka akun admin/teknisi/sales tidak bisa mengedit transaksi yang sudah lewat tanggal transaksi.</div>
+    <div class="text-sm">Dengan mengaktifkan ini maka akun admin/teknisi/sales bisa mengedit transaksi yang sudah lewat tanggal transaksi.</div>
     <div class="flex items-center mt-3">
         <div class="form-switch">
             <input type="checkbox" id="toggleEditTransaksi" class="sr-only" wire:model="is_edit_transaksi"
