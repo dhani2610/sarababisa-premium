d578ea4d3287a1185691791fdbc30fc48d4684b0
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-08-13 09:20:13 +0700

    fix : nota pengambilan perbaikan error

--- resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet.blade.php
+++ resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet.blade.php
@@ -224,11 +224,9 @@
                             @foreach (json_decode($items->biaya_j) as $key => $biaya)
                                 <ul style="margin: 0; padding: 0; margin-left: 10px; margin-top: 3px;">
                                     <li style="margin-bottom: 6px">
-                                        @if (isset($items->tindakan_servis[$key]))
-                                        {{ json_decode($items->tindakan_servis)[$key] . ' = Rp. ' . number_format($biaya) }}
-                                        @else
-                                        -                                            
-                                        @endif
+                                        {{ is_array(json_decode($items->tindakan_servis, true)) && isset(json_decode($items->tindakan_servis, true)[$key]) 
+                                        ? json_decode($items->tindakan_servis, true)[$key] . ' = Rp. ' . number_format($biaya) 
+                                        : '-' }}
                                     </li>
                                 </ul>
                             @endforeach
@@ -279,11 +277,9 @@
                             @foreach (json_decode($items->biaya_j) as $key => $biaya)
                                 <ul style="margin: 0; padding: 0; margin-left: 10px; margin-top: 3px;">
                                     <li style="margin-bottom: 6px">
-                                        @if (isset($items->tindakan_servis[$key]))
-                                        {{ json_decode($items->tindakan_servis)[$key] . ' = Rp. ' . number_format($biaya) }}
-                                        @else
-                                        -                                            
-                                        @endif
+                                        {{ is_array(json_decode($items->tindakan_servis, true)) && isset(json_decode($items->tindakan_servis, true)[$key]) 
+                                        ? json_decode($items->tindakan_servis, true)[$key] . ' = Rp. ' . number_format($biaya) 
+                                        : '-' }}
                                     </li>
                                 </ul>
                             @endforeach
@@ -322,11 +318,9 @@
                             @foreach (json_decode($items->biaya_j) as $key => $biaya)
                                 <ul style="margin: 0; padding: 0; margin-left: 10px; margin-top: 3px;">
                                     <li style="margin-bottom: 6px">
-                                        @if (isset($items->tindakan_servis[$key]))
-                                        {{ json_decode($items->tindakan_servis)[$key] . ' = Rp. ' . number_format($biaya) }}
-                                        @else
-                                        -                                            
-                                        @endif
+                                        {{ is_array(json_decode($items->tindakan_servis, true)) && isset(json_decode($items->tindakan_servis, true)[$key]) 
+                                        ? json_decode($items->tindakan_servis, true)[$key] . ' = Rp. ' . number_format($biaya) 
+                                        : '-' }}
                                     </li>
                                 </ul>
                             @endforeach
@@ -365,11 +359,9 @@
                            @foreach (json_decode($items->biaya_j) as $key => $biaya)
                                 <ul style="margin: 0; padding: 0; margin-left: 10px; margin-top: 3px;">
                                     <li style="margin-bottom: 6px">
-                                        @if (isset($items->tindakan_servis[$key]))
-                                        {{ json_decode($items->tindakan_servis)[$key] . ' = Rp. ' . number_format($biaya) }}
-                                        @else
-                                        -                                            
-                                        @endif
+                                        {{ is_array(json_decode($items->tindakan_servis, true)) && isset(json_decode($items->tindakan_servis, true)[$key]) 
+                                        ? json_decode($items->tindakan_servis, true)[$key] . ' = Rp. ' . number_format($biaya) 
+                                        : '-' }}
                                     </li>
                                 </ul>
                             @endforeach
