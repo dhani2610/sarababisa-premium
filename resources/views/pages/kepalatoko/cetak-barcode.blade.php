ef4aaedaea0539fa0768d12ff23cdb37bbdeb4ef
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-08-17 00:45:56 +0700

    fix : barcode

--- resources/views/pages/kepalatoko/cetak-barcode.blade.php
+++ resources/views/pages/kepalatoko/cetak-barcode.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <style>
@@ -8,18 +9,28 @@
             font-family: sans-serif;
             font-size: 14px;
         }
+
         .product-name {
             font-weight: bold;
             margin-bottom: 8px;
         }
+
         img {
             max-width: 100%;
         }
     </style>
 </head>
+
 <body>
-    <div class="product-name">{{ $product->product_name }} {{ $product->kondisi }}</div>
+    @if ($product->categories_id == 1)
+        <div class="product-name">
+            {{ $item->product_name }} {{ $item->kondisi }} {{ $item->warna }} {{ $item->ram }} / @if ($item->capacity != null)
+        </div>
+    @else
+        <div class="product-name">{{ $product->product_name }} {{ $product->kondisi }}</div>
+    @endif
     <img src="data:image/png;base64,{{ $barcodeData }}" alt="barcode">
     <div>{{ $product->product_code }}</div>
 </body>
+
 </html>
