05dd62516399c991050df8134b71e21fe3cfccfc
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-09-23 10:06:14 +0700

    fix : update transaksi

--- app/Http/Controllers/AdminToko/TransaksiServisLangsungController.php
+++ app/Http/Controllers/AdminToko/TransaksiServisLangsungController.php
@@ -28,7 +28,7 @@ class TransaksiServisLangsungController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function stores(Request $request)
     {
         // dd($request->all());
         $nomor_servis = '' . mt_rand(date('Ymd00'), date('Ymd99')).rand(10,90);
