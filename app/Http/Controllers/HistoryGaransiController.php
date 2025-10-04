45bd324784d4c459b59bad11072aec63cea8960d
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-10-04 19:43:55 +0700

    fix : update error history

--- app/Http/Controllers/HistoryGaransiController.php
+++ app/Http/Controllers/HistoryGaransiController.php
@@ -88,17 +88,18 @@ class HistoryGaransiController extends Controller
 
                 $service = ServiceTransaction::find($request->service_id);
 
+                $harga_jual = $spareparts->harga_jual ?? 0;
                 $nama_pelanggan = Customer::find($service->customers_id)->nama;
                 $order = new Order();
                 $order->customers_id = $service->customers_id;
                 $order->users_id = $request->teknisi_id;
                 $order->order_date = Carbon::today()->locale('id')->translatedFormat('d F Y');
                 $order->total_products = 1;
-                $order->sub_total = $spareparts->harga_jual;
+                $order->sub_total = $harga_jual;
                 $order->invoice_no = '' . mt_rand(date('Ymd00'), date('Ymd99'));
                 $order->nama_pelanggan = $nama_pelanggan;
                 $order->payment_method = "Tunai";
-                $order->pay = $spareparts->harga_jual;
+                $order->pay = $harga_jual;
                 $order->due = 0;
                 $order->is_approve = 'Setuju';
                 $order->tgl_disetujui = Carbon::today();
@@ -117,13 +118,13 @@ class HistoryGaransiController extends Controller
                 $orderDetail->products_id = $spareparts->id;
                 $orderDetail->product_name = $spareparts->product_name;
                 $orderDetail->quantity = (int)$row['qty'];
-                $orderDetail->price = $spareparts->harga_jual;
-                $orderDetail->total = $spareparts->harga_jual;
-                $orderDetail->sub_total = $spareparts->harga_jual;
+                $orderDetail->price = $harga_jual;
+                $orderDetail->total = $harga_jual;
+                $orderDetail->sub_total = $harga_jual;
                 $orderDetail->modal = $spareparts->harga_modal;
-                $orderDetail->profit = $spareparts->harga_jual - $spareparts->harga_modal;
+                $orderDetail->profit = $harga_jual - $spareparts->harga_modal;
                 $orderDetail->persen_sales = $persen_sales;
-                $orderDetail->profit_toko = ($spareparts->harga_jual - $spareparts->harga_modal) - ($spareparts->harga_jual - $spareparts->harga_modal) / 100 * $persen_sales;
+                $orderDetail->profit_toko = ($harga_jual - $spareparts->harga_modal) - ($spareparts->harga_jual - $spareparts->harga_modal) / 100 * $persen_sales;
                 $orderDetail->garansi = date('Y-m-d');
                 $orderDetail->product_discount_amount = 0;
                 $orderDetail->save();
