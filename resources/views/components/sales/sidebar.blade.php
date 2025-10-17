9720b6e3c4a64bbee3d0b78b1667a841a8159dc1
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-10-17 09:32:36 +0700

    fix : hide menu sales karna tidak terpakai

--- resources/views/components/sales/sidebar.blade.php
+++ resources/views/components/sales/sidebar.blade.php
@@ -88,7 +88,7 @@
                         </a>
                         <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
                             <ul class="pl-9 mt-1 @if(!in_array(Request::segment(1), ['produk'])){{ 'hidden' }}@endif" :class="open ? '!block' : 'hidden'">
-                                <li class="mb-1 last:mb-0">
+                                {{-- <li class="mb-1 last:mb-0">
                                     <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales-kategori.index')){{ '!text-indigo-500' }}@endif" href="{{ route('sales-kategori.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Kategori Produk</span>
                                     </a>
@@ -97,7 +97,7 @@
                                     <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales-sub-kategori.index')){{ '!text-indigo-500' }}@endif" href="{{ route('sales-sub-kategori.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Sub Kategori Produk</span>
                                     </a>
-                                </li>
+                                </li> --}}
                                 <li class="mb-1 last:mb-0">
                                     <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('sales-item.index', 'sales-handphone.index', 'sales-sparepart.index', 'sales-aksesoris.index', 'sales-tool.index')){{ '!text-indigo-500' }}@endif" href="{{ route('sales-item.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Item Produk</span>
