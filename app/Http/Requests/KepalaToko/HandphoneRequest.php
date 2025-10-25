c754b380b1c47ab59421b3ea50ddf9e68c67282b
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-10-26 00:33:46 +0700

    fix : validasi exist error input produk

--- app/Http/Requests/KepalaToko/HandphoneRequest.php
+++ app/Http/Requests/KepalaToko/HandphoneRequest.php
@@ -28,21 +28,13 @@ class HandphoneRequest extends FormRequest
             'product_name' => 'max:100',
             'product_code' => 'max:100',
             'nomor_seri' => 'max:100|required|unique:products,nomor_seri',
-            'categories_id' => [
-                'exists:categories,id',
-            ],
-            'brands_id' => [
-                'exists:brands,id',
-            ],
-            'model_series_id' => [
-                'exists:model_series,id',
-            ],
+            'categories_id' => 'required',
+            'brands_id' => 'required',
+            'model_series_id' => 'required',
             'category_name' => 'max:100',
             'warna' => 'max:100',
             'ram' => 'max:100',
-            'capacities_id' => [
-                'exists:capacities,id',
-            ],
+            'capacities_id' => 'required',
             'stok' => 'required|max:100',
             'harga_modal' => 'required|max:100',
             'harga_jual' => 'required|max:100',
--- app/Http/Requests/KepalaToko/ProductRequest.php
+++ app/Http/Requests/KepalaToko/ProductRequest.php
@@ -27,23 +27,13 @@ class ProductRequest extends FormRequest
             'product_name' => 'required|unique:products,product_name',
             'product_code' => 'max:100',
             'nomor_seri' => 'max:100',
-            'categories_id' => [
-                'required',
-            ],
-            'sub_categories_id' => [
-                'required',
-            ],
-            'brands_id' => [
-                'required',
-            ],
-            'model_series_id' => [
-                'required',
-            ],
+            'categories_id' => 'required',
+            'sub_categories_id' => 'required',
+            'brands_id' => 'required',
+            'model_series_id' => 'required',
             'category_name' => 'max:100',
             'ram' => 'max:100',
-            'capacities_id' => [
-                'required',
-            ],
+            'capacities_id' => 'required',
             'stok' => 'required|max:100',
             'harga_modal' => 'required|max:100',
             'harga_jual' => 'required|max:100',
