c589511b924a654f71af7d034e308c3273f3e0c4
andakagdv <andaka@goodeva.co.id>
2025-11-03 23:08:22 +0700

    fix : optimasi datatable

--- app/Http/Controllers/KepalaToko/TransaksiProdukController.php
+++ app/Http/Controllers/KepalaToko/TransaksiProdukController.php
@@ -37,10 +37,15 @@ class TransaksiProdukController extends Controller
 
     public function data(Request $request)
     {
+        $limit = $request->get('limit', 200);
+        $offset = $request->get('offset', 0);
+    
         $orders = Order::select('orders.*', 'order_details.modal')
             ->join('order_details', 'orders.id', '=', 'order_details.orders_id')
             ->with(['user', 'customer'])
             ->orderByRaw('is_approve IS NULL DESC')
+            ->skip($offset)
+            ->take($limit)
             ->latest();
 
         return DataTables::of($orders)
