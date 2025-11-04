4718eae791f39a0cd611136f4bc4b87a450ceedc
andakagdv <andaka@goodeva.co.id>
2025-11-04 11:25:09 +0700

    fix : otomatis modal

--- resources/views/livewire/history-garansi.blade.php
+++ resources/views/livewire/history-garansi.blade.php
@@ -186,7 +186,7 @@
                                 <div>
                                     <label class="block text-sm font-medium mb-1">Total Modal Tindakan</label>
                                     <input type="number" name="total_biaya_tindakan" id="total_biaya_tindakan"
-                                        value="0" class="form-input w-full" required>
+                                        value="0" class="form-input w-full" required onkeyup="updateTotalModal()">
                                 </div>
 
                                 <hr>
@@ -739,6 +739,13 @@ document.addEventListener('alpine:init', () => {
         });
     </script>
     <script>
+        function updateTotalModal() {
+            console.log('====================================');
+            console.log('onchange');
+            console.log('====================================');
+            const tindakan = parseFloat(document.getElementById('total_biaya_tindakan').value) || 0;
+            document.getElementById('total_biaya').value = tindakan;
+        }
         document.addEventListener("DOMContentLoaded", function() {
             let tindakanList = @json($serviceActions); // pastikan kamu kirim $serviceActions dari controller
             let tindakanContainer = document.getElementById("tindakanContainer");
