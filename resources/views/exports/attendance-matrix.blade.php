6dc204a4bdbc5e47eac04d8a08de0119f8ef3e28
andakagdv <andaka@goodeva.co.id>
2025-11-04 09:26:53 +0700

    fix : perbaikan waktu di absensi export

--- resources/views/exports/attendance-matrix.blade.php
+++ resources/views/exports/attendance-matrix.blade.php
@@ -32,8 +32,8 @@
                         $masuk = $record->firstWhere('type', 'masuk');
                         $pulang = $record->firstWhere('type', 'pulang');
                     @endphp
-                    <td>{{ $masuk ? \Carbon\Carbon::parse($masuk->waktu)->format('H:i') : '-' }}</td>
-                    <td>{{ $pulang ? \Carbon\Carbon::parse($pulang->waktu)->format('H:i') : '-' }}</td>
+                    <td>{{ $masuk ? \Carbon\Carbon::parse($masuk->created_at)->format('H:i') : '-' }}</td>
+                    <td>{{ $pulang ? \Carbon\Carbon::parse($pulang->created_at)->format('H:i') : '-' }}</td>
                 @endforeach
             </tr>
         @endforeach
