f538a28a11d94901e49a91ad4f2091b96e7ffc73
andakagdv <andaka@goodeva.co.id>
2025-11-18 17:29:02 +0700

    feat : multi cabang

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -68,7 +68,7 @@ class PortalController extends Controller
         $data['products'] = $query->paginate($perPage)->appends($request->all());
 
         $data['total_products'] = Product::where('is_portal',1)->whereIn('categories_id', $data['productCategory']->pluck('id'))->count();
-        $data['toko_setting'] = StoreSetting::where('cabang_id',getCabangId())->first();
+        $data['toko_setting'] = StoreSetting::find(1);
         $data['kepala_toko_setting'] = User::find(1);
         $data['pelanggan'] = Customer::count();
         $data['galleries'] = Gallery::orderBy('created_at', 'desc')->get();
