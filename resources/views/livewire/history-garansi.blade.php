bab5cbb4ddf9390993b606357f8cdfa3ffc707b6
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-11-20 23:32:30 +0700

    fix : perbaikan error cetak history

--- resources/views/livewire/history-garansi.blade.php
+++ resources/views/livewire/history-garansi.blade.php
@@ -511,8 +511,6 @@
                                                 </button>
                                             </a>
                                             @if ($item->status == 1 || $item->status == 2)
-
-                                            @endif
                                             <a href="{{ route('history-garansi.cetak-inject', $item->id) }}" target="_blank">
                                                 <button class="text-slate-400 hover:text-slate-500 rounded-full" title="Ubah">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -523,6 +521,7 @@
                                                     </svg>
                                                 </button>
                                             </a>
+                                            @endif
                                         </div>
                                             <div x-data="{ modalOpen: false }">
                                                 <button class="text-rose-500 hover:text-rose-600 rounded-full"
