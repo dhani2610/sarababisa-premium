554a8c7ef92db2ae76c987103c489a5c2ad79ed4
andakagdv <andaka@goodeva.co.id>
2025-11-21 16:05:36 +0700

    feat : transfer stok

--- resources/views/components/kepalatoko/sidebar.blade.php
+++ resources/views/components/kepalatoko/sidebar.blade.php
@@ -188,7 +188,7 @@
                                     </a>
                                 </li>
                                 <li class="mb-1 last:mb-0">
-                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('transfer-stok.index')){{ '!text-indigo-500' }}@endif" href="{{ route('item.index') }}">
+                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('transfer-stok.index')){{ '!text-indigo-500' }}@endif" href="{{ route('transfer-stok.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Transfer Stok</span>
                                     </a>
                                 </li>
