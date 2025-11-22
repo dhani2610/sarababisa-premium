ec35ebddd1c720d0229d8d90921023597e5dcdf6
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2025-11-22 21:00:58 +0700

    fix : error validasi produk

--- app/Http/Requests/KepalaToko/InventoryRequest.php
+++ app/Http/Requests/KepalaToko/InventoryRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\KepalaToko;
 
 use Illuminate\Foundation\Http\FormRequest;
-use function App\Helpers\getCabangId;
 use App\Models\Inventory;
 
 class InventoryRequest extends FormRequest
