bec5a03a0465abc1dbfe32868596719a2629161b
andakagdv <andaka@goodeva.co.id>
2025-11-28 10:30:23 +0700

    fix : set where disetujui di laporan admin

--- app/Http/Controllers/KepalaToko/LaporanAdminController.php
+++ app/Http/Controllers/KepalaToko/LaporanAdminController.php
@@ -37,31 +37,27 @@ class LaporanAdminController extends Controller
 
         // Mengambil data servis
         $services = ServiceTransaction::with('brand', 'modelserie')->where('admin_id', $request->admin_id)->where('status_servis', 'Sudah Diambil')
-            ->where('is_approve', 'Setuju')
-            ->whereDate('tgl_disetujui', '>=', $start_date)
-            ->whereDate('tgl_disetujui', '<=', $end_date)
-            ->orderBy('tgl_disetujui', 'asc')
+            ->whereDate('tgl_ambil', '>=', $start_date)
+            ->whereDate('tgl_ambil', '<=', $end_date)
+            ->orderBy('tgl_ambil', 'asc')
             ->get();
 
         // Menghitung total tindakan
         $total_tindakan = ServiceTransaction::where('admin_id', $request->admin_id)->where('status_servis', 'Sudah Diambil')
-            ->where('is_approve', 'Setuju')
-            ->whereDate('tgl_disetujui', '>=', $start_date)
-            ->whereDate('tgl_disetujui', '<=', $end_date)
+            ->whereDate('tgl_ambil', '>=', $start_date)
+            ->whereDate('tgl_ambil', '<=', $end_date)
             ->count();
 
         // Menghitung total biaya servis
         $total_biaya_servis = ServiceTransaction::where('status_servis', 'Sudah Diambil')->where('admin_id', $request->admin_id)
-            ->where('is_approve', 'Setuju')
-            ->whereDate('tgl_disetujui', '>=', $start_date)
-            ->whereDate('tgl_disetujui', '<=', $end_date)
+            ->whereDate('tgl_ambil', '>=', $start_date)
+            ->whereDate('tgl_ambil', '<=', $end_date)
             ->sum('biaya');
 
         // Menghitung total profit servis
         $total_profit_servis = ServiceTransaction::where('admin_id', $request->admin_id)->where('status_servis', 'Sudah Diambil')
-            ->where('is_approve', 'Setuju')
-            ->whereDate('tgl_disetujui', '>=', $start_date)
-            ->whereDate('tgl_disetujui', '<=', $end_date)
+            ->whereDate('tgl_ambil', '>=', $start_date)
+            ->whereDate('tgl_ambil', '<=', $end_date)
             ->sum('profit');
 
         // Menghitung total bonus servis
