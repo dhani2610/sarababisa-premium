9f91be85621ca9f689446279607e7049c194cc92
andakagdv <andaka@goodeva.co.id>
2025-12-24 10:37:31 +0700

    fix : terbilang nota

--- resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet-new.blade.php
+++ resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet-new.blade.php
@@ -388,7 +388,7 @@
     <table>
         <tr>
             <th class="text-right w-75"></th>
-            <th class="text-right w-75" colspan="3">
+            <th class="text-right w-75" colspan="2">
                 <i><span style="font-size: 10px; font-weight: normal;"><b>Terbilang : {{ terbilang($totalWithPpn) }}</b></span></i>
             </th>
         </tr>
