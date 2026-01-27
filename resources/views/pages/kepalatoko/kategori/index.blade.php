dcd5069024781c6649c1a9b937fb595e61d1c572
andakagdv <andaka@goodeva.co.id>
2026-01-27 09:32:21 +0700

    fix : hide tombol tambah kategori

--- resources/views/pages/kepalatoko/kategori/index.blade.php
+++ resources/views/pages/kepalatoko/kategori/index.blade.php
@@ -17,12 +17,12 @@
 
             <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
                 <div x-data="{ modalOpen: false }">
-                    <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white" @click.prevent="modalOpen = true" aria-controls="tambah-modal">
+                    {{-- <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white" @click.prevent="modalOpen = true" aria-controls="tambah-modal">
                         <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                             <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                         </svg>
                         <span class="ml-2">Tambah Kategori</span>
-                    </button>
+                    </button> --}}
 
                     <div class="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity" x-show="modalOpen" x-cloak></div>
                     <div class="fixed inset-0 z-50 flex items-center justify-center px-4" x-show="modalOpen" x-cloak>
