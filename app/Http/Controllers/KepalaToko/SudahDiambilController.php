f6717d10485106e69a219d6f4ae4c31bf9b69b7d
andakagdv <andaka@goodeva.co.id>
2026-01-30 23:09:37 +0700

    fix : perbaikan kirim fontee

--- app/Http/Controllers/KepalaToko/SudahDiambilController.php
+++ app/Http/Controllers/KepalaToko/SudahDiambilController.php
@@ -99,7 +99,7 @@ class SudahDiambilController extends Controller
             // ✅ Hubungi (whatsapp + fonnte)
             ->addColumn('hubungi', function ($row) {
                  if (!$row->customer) return '';
-                    $toko = StoreSetting::first();
+                    $toko = StoreSetting::where('cabang_id',$row->cabang_id)->first();
                     $nomor = $row->customer->nomor_hp;
                     $nomorwa = preg_replace('/^08/', '628', $nomor);
                     $fonnteToken = $toko->fonnte ?? null;
