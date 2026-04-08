01a3c6d34864c8f6396cdffd228c92d210483fbb
andakagdv <andaka@goodeva.co.id>
2026-04-08 21:59:00 +0700

    update terbilang

--- resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet-new.blade.php
+++ resources/views/pages/kepalatoko/servis/notapengambilan-cetak-inkjet-new.blade.php
@@ -395,8 +395,8 @@
         <tr>
             {{-- <th class="text-right w-75"></th> --}}
             <th class="text-right w-75" colspan="5">
-                {{-- <i><span style="font-size: 10px; font-weight: normal;"><b>Terbilang : {{ terbilang($totalWithPpn) }}</b></span></i> --}}
-                <i><span style="font-size: 10px; font-weight: normal;text-align: right;" ><b>Terbilang :  Tiga Juta Empat Ratus Lima Puluh Ribu</b></span></i>
+                <i><span style="font-size: 10px; font-weight: normal;"><b>Terbilang : {{ terbilang($totalWithPpn) }}</b></span></i>
+                {{-- <i><span style="font-size: 10px; font-weight: normal;text-align: right;" ><b>Terbilang :  Tiga Juta Empat Ratus Lima Puluh Ribu</b></span></i> --}}
             </th>
         </tr>
         @if ($items->catatan != null)
