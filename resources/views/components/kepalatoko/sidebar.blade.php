b5b206f4fc485a93e24e8ef12dd74ef2ac87d57b
andakagdv <andaka@goodeva.co.id>
2026-05-06 14:14:21 +0700

    fix : update typo sidebar metode

--- resources/views/components/kepalatoko/sidebar.blade.php
+++ resources/views/components/kepalatoko/sidebar.blade.php
@@ -285,7 +285,7 @@
                                 @endif
                                 <li class="mb-1 last:mb-0">
                                     <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('metode-pembayaran.index')){{ '!text-indigo-500' }}@endif" href="{{ route('metode-pembayaran.index') }}">
-                                        <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Matode Pembayaran</span>
+                                        <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Metode Pembayaran</span>
                                     </a>
                                 </li>
                                 {{-- <li class="mb-1 last:mb-0">
